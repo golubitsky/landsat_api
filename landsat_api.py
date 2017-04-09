c73c98e897eeb8e0076a384e94d7caef6e8a4948
Mike Golubitsky <mike617@gmail.com>
2017-04-09 10:46:57 -0700

    basic cors example

--- landsat_api.py
+++ landsat_api.py
@@ -1,33 +1,35 @@
 import feedparser as fp
 from flask import Flask
-from flask.ext.cors import CORS, cross_origin
+from flask import jsonify
+from flask_cors import CORS
+
 app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+CORS(app)
+
 
 @app.route('/')
 def helloworld():
-	return 'Hello!'
+    return 'Hello!'
+
 
 @app.route('/api/landsat')
 def landsat_rss():
-	feed = fp.parse('https://landsat.usgs.gov/landsat/rss/Landsat_L1T.rss')
-	number_of_entries = len(feed.entries)
+    feed = fp.parse('https://landsat.usgs.gov/landsat/rss/Landsat_L1T.rss')
 
     response = jsonify([serialize_landsat_entry(entry) for entry in feed.entries])
-    response.headers.add('Access-Control-Allow-Origin', '*')
+    # response.headers.add('Access-Control-Allow-Origin', '*')
 
     return response
 
 
 def serialize_landsat_entry(entry):
-	return {
+    return {
         'title': entry.title,
         'latitude': float(entry.geo_lat),
         'longitude': float(entry.geo_long),
         'imageLink': entry.id,
     }
 
-if __name__ == '__main__':
-	app.run(debug=True, use_reloader=True)
 
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
